ee3c4eb9f9e0b8f60ac1380e95e91e94a16f0c53
Ben Vandervalk <ben.vvalk@gmail.com>
2025-09-24 16:01:36 -0400

    elisp-tred: move mode-related functions to top of file [refactor]
    I believe it is more conventional to declare the main mode-related
    functions at the top of the file, rather than at the bottom.

--- elisp-tred.el
+++ elisp-tred.el
@@ -7,10 +7,33 @@
 
 (defvar-local elisp-tred--tree-buffer nil)
 
+(define-minor-mode elisp-tred-mode
+  "Minor mode for Elisp tree editing."
+  :lighter "ET"
+  (if elisp-tred-mode
+      (elisp-tred--enable)
+    (elisp-tred--kill-tree-buffer)))
+
 (define-derived-mode elisp-tred--tree-mode special-mode
   "TM"
   "Mode for displaying lisp code as a tree."
   nil)
+
+(defun elisp-tred--enable ()
+  (message "enabling elisp-tred")
+  (unless (treesit-ready-p 'elisptred)
+	(if (not (treesit-language-available-p 'elisptred))
+        (user-error "Cannot find treesit grammar for elisptred")
+	  (treesit-create-parser 'elisptred)))
+  (unless (buffer-live-p elisp-tred--tree-buffer)
+    (setq-local elisp-tred--tree-buffer
+                (get-buffer-create
+                 (format "*elisp-tred buffer for %s"
+                         (buffer-name))))
+    (with-current-buffer elisp-tred--tree-buffer
+      (elisp-tred--tree-mode)))
+  (display-buffer elisp-tred--tree-buffer)
+  (elisp-tred-refresh-tree-buffer))
 
 (defun elisp-tred--kill-tree-buffer ()
   (when (buffer-live-p elisp-tred--tree-buffer)
@@ -99,26 +122,3 @@
             (let ((form (nth i top-level-forms))
                   (is-last (= i (1- (length top-level-forms)))))
               (elisp-tred--render-tree form nil is-last))))))))
-
-(defun elisp-tred--enable ()
-  (message "enabling elisp-tred")
-  (unless (treesit-ready-p 'elisptred)
-	(if (not (treesit-language-available-p 'elisptred))
-        (user-error "Cannot find treesit grammar for elisptred")
-	  (treesit-create-parser 'elisptred)))
-  (unless (buffer-live-p elisp-tred--tree-buffer)
-    (setq-local elisp-tred--tree-buffer
-                (get-buffer-create
-                 (format "*elisp-tred buffer for %s"
-                         (buffer-name))))
-    (with-current-buffer elisp-tred--tree-buffer
-      (elisp-tred--tree-mode)))
-  (display-buffer elisp-tred--tree-buffer)
-  (elisp-tred-refresh-tree-buffer))
-
-(define-minor-mode elisp-tred-mode
-  "Minor mode for Elisp tree editing."
-  :lighter "ET"
-  (if elisp-tred-mode
-      (elisp-tred--enable)
-    (elisp-tred--kill-tree-buffer)))
