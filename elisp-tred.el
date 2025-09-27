0a58f4ed258ca9a1e2c1f4b19fee5fbd8d191efd
Ben Vandervalk <ben.vvalk@gmail.com>
2025-09-27 14:59:59 -0400

    elisp-tred: change tree node label when expanded/collapsed [feat]
    Set up a callback to update the text label for a tree node when the
    node is expanded or collapsed.
    
    What I have in mind is to show the full lisp code in a single line
    when a node is collapsed, but just show the opening paren ("(") when
    the node is expanded (since the remainder of the code will be visible
    by looking at the child nodes). That feature is not implemented yet --
    this commit just implements the callback with some silly label
    changes, to demonstrate that the dynamic label-changing works.

--- elisp-tred.el
+++ elisp-tred.el
@@ -31,7 +31,11 @@
                  (format "*elisp-tred buffer for %s"
                          (buffer-name))))
     (with-current-buffer elisp-tred--tree-buffer
-      (elisp-tred--tree-mode)))
+      (elisp-tred--tree-mode)
+      ;; Register a callback to update the labels of certain tree
+      ;; nodes, when they are expanded or collapsed.
+      (setq-local tree-widget-after-toggle-functions
+                  '(elisp-tred--update-label))))
   (display-buffer elisp-tred--tree-buffer)
   (elisp-tred-refresh-tree-buffer))
 
@@ -39,24 +43,45 @@
   (when (buffer-live-p elisp-tred--tree-buffer)
     (kill-buffer elisp-tred--tree-buffer)))
 
+(defun elisp-tred--update-label (widget)
+  "Update the text label for the given tree node WIDGET.
+
+This function allows showing different labels on a tree node,
+depending on whether the node is collapsed or expanded."
+  (let* ((node (widget-get widget :treesit-node))
+         (node-type (treesit-node-type node)))
+    (when t ;(eq node-type "list")
+      (let* ((button (widget-get widget :node))
+             (open (widget-get widget :open))
+             (new-label (if open "expanded!" "collapsed")))
+        (widget-put button :tag new-label)))))
+
 (defun elisp-tred--get-label (node)
   (let ((node-type (treesit-node-type node)))
     (concat (pcase node-type
-              ("list" "collapsed")
               ("symbol" (treesit-node-text node))
               (")" ")"))
             (format " [%s]" node-type))))
 
 (defun elisp-tred--get-tree-widget (node)
+  "Return the tree widget definition corresponding to treesit node
+NODE.
+
+The tree widget definition is used render the treesit nodes as
+collapsible UI widget in the tree buffer."
   `(tree-widget
     :node (push-button
            :tag ,(elisp-tred--get-label node)
-           :button-face 'default
+           :button-face default
            :format "%[%t%]\n")
     :treesit-node ,node
-    :expander elisp-tred--expander))
+    :expander elisp-tred--get-child-widgets))
 
-(defun elisp-tred--expander (widget)
+(defun elisp-tred--get-child-widgets (widget)
+  "Get the widget definitions for the children of the given tree node
+WIDGET.
+
+This function is called when expanding a tree node in the UI."
   (let ((node (widget-get widget :treesit-node)))
     (mapcar 'elisp-tred--get-tree-widget
             (elisp-tred--get-children node))))
