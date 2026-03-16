ecad5c2068f76525294d6e0b192c2dbc40ad8e12
Ben Vandervalk <ben.vvalk@gmail.com>
2026-03-16 14:09:01 -0400

    tests: add `test-edit-delete-open-paren-simple` [test]

--- elisp-tred-test.el
+++ elisp-tred-test.el
@@ -1,6 +1,16 @@
 (require 'elisp-tred)
 (require 'ert)
 
+(defun elisp-tred--test-buffer-create (quoted-elisp-form)
+  "Create an elisp-tred buffer whose entire content is
+QUOTED-ELISP-FORM."
+  (let ((buffer (get-buffer-create "*elisp-tred-test*")))
+    (with-current-buffer buffer
+      (erase-buffer)
+      (prin1 quoted-elisp-form (current-buffer))
+      (elisp-tred-mode))
+    buffer))
+
 (ert-deftest elisp-tred--test-render-list-simple ()
   "Test tree rendering for simple list."
   (should
@@ -8,3 +18,27 @@
     (elisp-tred--render-elisp-form-to-string '(a b))
     (concat "╰─ (a\n"
             "   ╰─ b)"))))
+
+(ert-deftest elisp-tred--test-edit-delete-open-paren-simple ()
+  (let ((buffer (elisp-tred--test-buffer-create '(a b))))
+    ;; test buffer state before
+    (should
+     (string=
+      (elisp-tred--render-buffer-to-string buffer)
+      (concat "╰─ (a\n"
+              "   ╰─ b)")))
+	;; perform edit: delete open paren '('
+    (with-current-buffer buffer
+	  (goto-char (point-min))
+      (delete-char 1)
+      (elisp-tred--update-buffer))
+    ;; test buffer state after
+    ;;
+    ;; NOTE: unbalanced closing paren ')' appears on its own line
+    ;; because it is a treesit error node
+    (should
+     (string=
+      (elisp-tred--render-buffer-to-string buffer)
+      (concat "├─ a\n"
+              "├─ b\n"
+              "╰─ )")))))
